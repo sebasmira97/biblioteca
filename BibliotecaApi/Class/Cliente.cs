295fcf68a91a6deca76126956d0216cfec5a0eca
Emmanuel Villada Suarez <emmanuelvillada310016@correo.itm.edu.co>
2023-11-20 00:05:07 -0500

    api created with model

---
+++ BibliotecaApi/Class/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BibliotecaApi.Class
+{
+    public class Cliente
+    {
+        public DbSet<Cliente> Clientes { get; set;}
+
+        DatabaseContext = db
+    }
+}
---
+++ BibliotecaApi/Controllers/ClientesController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BibliotecaApi.Controllers
+{
+    public class ClientesController : Controller
+    {
+        // GET: Clientes
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Clientes/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Clientes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Clientes/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(
+    [Bind("EnrollmentDate,FirstMidName,LastName")] Cliente cliente)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.Add(student);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch (DbUpdateException /* ex */)
+            {
+                //Log the error (uncomment ex variable name and write a log.
+                ModelState.AddModelError("", "Unable to save changes. " +
+                    "Try again, and if the problem persists " +
+                    "see your system administrator.");
+            }
+            return View(student);
+        }
+
+        // GET: Clientes/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Clientes/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Clientes/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Clientes/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
